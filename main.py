6a70e158970cc6c64e18cb9c2de78f2db57408fe
= <=>
2022-08-13 23:10:02 +0000

    Adding importation statement

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import math as M
+import reseau_amont
+import transformateur
+import liaison
+import machine_tournante
+
 reseau = reseau_amont.ReseauAmont(0, 0, 0, [20000, 'V'], [(500 * M.pow(10, 6)), 'VA'])
 transfo = transformateur.Transformateur(0, 0, 0, [410, 'V'], 15, [1000, 'VA'], 0)
 
 liaisons = liaison.Liaison(0, 'cuivre', 0, 50, [False, ''], [True, 'htb'], 2000) 
 machine = machine_tournante.MachineTounante(0, 0, 0, [20000, 'V'], [M.pow(10, 6), 'VA'], 15)
+
 
 #print(reseau.get_impedance() * 0.2)
 
@@ -14,5 +21,3 @@ icc = reseau.tension['valeur'] / (M.sqrt(3) * zcc)
 #
 print("icc=" ,icc)
 print("zcc=" ,zcc)
-
-
